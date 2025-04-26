f38d642426a6cc63a08219522fd73ff5a36ed6cd
Isaac Machado <isaacmachado.profissional@gmail.com>
2025-04-26 11:40:04 -0300

    feat: skills

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,5 +1,9 @@
-import "../css/style.css";
+import '../css/style.css'
+
 import { startTypewriter } from './typewriter.js';
+import { initGradientRestart } from "./gradientRestart.js";
+
+initGradientRestart();
 
 startTypewriter("typewriter", [
     "Desenvolvedor WEB. ",
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 const path = require('path');
 const porta = 9001;
@@ -9,42 +9,86 @@ module.exports = {
   entry: './src/javascript/index.js', // Arquivo de entrada principal
   output: {
     path: path.resolve(__dirname, 'dist'), // Diretório de saída
-    filename: 'bundle.js', // Arquivo gerado
+    filename: 'bundle.js', // Arquivo JavaScript gerado
+    publicPath: '/', // Caminho absoluto para os assets gerados
   },
   module: {
     rules: [
       {
-        test: /\.css$/, // Arquivos CSS
+        test: /\.css$/, // Processa arquivos CSS
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
-        test: /\.(png|jpg|gif)$/, // Arquivos de imagem
-        type: 'asset/resource', 
+        test: /\.(png|jpg|gif)$/, // Processa imagens
+        type: 'asset/resource',
       },
       {
-        test: /\.html$/, // arquivos HTML
+        test: /\.html$/, // Processa arquivos HTML (caso precise importar partes deles)
         use: ['html-loader'],
       },
     ],
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(), // Plugin necessário para HMR
+    new webpack.HotModuleReplacementPlugin(), // Para HMR (Hot Module Replacement) em desenvolvimento
+    // Cria o index.html com injeção automática do bundle e dos assets processados
     new HtmlWebpackPlugin({
-        template: './index.html',
-        filename: 'index.html',
-        inject: true,
-      }),
+      template: './public/index.html',
+      filename: 'index.html',
+      inject: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/contact.html',
+      filename: 'contact.html',
+      inject: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/formation.html',
+      filename: 'formation.html',
+      inject: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/skills.html',
+      filename: 'skills.html',
+      inject: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/projects.html',
+      filename: 'projects.html',
+      inject: true,
+    }),
+
+
+    // Copia os arquivos estáticos da pasta public para o diretório de saída,
+    // ignorando os HTMLs que já foram processados pelo HtmlWebpackPlugin
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: './public',
+          to: '.',
+          globOptions: {
+            ignore: [
+              '**/index.html',
+              '**/contact.html',
+              '**/formation.html',
+              '**/skills.html',
+              '**/projects.html',
+
+            ],
+          },
+        },
+        
+      ],
+    }),
     new MiniCssExtractPlugin({
-      filename: '[name].css', // Nome do arquivo CSS gerado
-      chunkFilename: '[id].css', // Nome dos chunks (opcional)
+      filename: '[name].css',     // Nome do arquivo CSS principal
+      chunkFilename: '[id].css',  // Nome dos chunks, se houver
     }),
   ],
   devServer: {
-    static: path.resolve(__dirname, ''),
-    port: porta, // Porta do servidor
-    open: true, // Abre automaticamente o navegador
-    hot: true, // Habilita o Hot Module Replacement (HMR)
+    static: path.resolve(__dirname, 'dist'), // Serve os arquivos do diretório de saída (dist)
+    port: porta,                             // Porta do servidor
+    open: true,                              // Abre o navegador automaticamente
+    hot: true,                               // Ativa o HMR
   },
-  
-  mode: 'development', 
+  mode: 'development',
 };
